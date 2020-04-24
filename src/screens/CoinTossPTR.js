64fa006c0574a97e3ea1eb2719f32076f511bebb
rody08 <33561437+rody08@users.noreply.github.com>
2020-04-24 01:06:08 -0500

    coinTossPTR updated

--- src/screens/CoinTossPTR.js
+++ src/screens/CoinTossPTR.js
@@ -2,16 +2,13 @@ import { View as GraphicsView } from 'expo-graphics';
 import ExpoTHREE, { THREE } from 'expo-three';
 import { EventRegister } from 'react-native-event-listeners'
 import React, { useState } from 'react';
-import { Animated, StyleSheet, Text, View, Button } from "react-native";
+import { Animated, StyleSheet, styles, View, Button } from "react-native";
 
 export default class App extends React.Component {
-   /*
+
     animated = new Animated.Value(0);
       constructor(props) {
           super(props)
-          this.state = {
-              data: 'no data',
-          }
         };
 
     animate() {
@@ -27,30 +24,18 @@ export default class App extends React.Component {
     // Create an `ExpoGraphics.View` covering the whole screen, tell it to call our
     // `onContextCreate` function once it's initialized.
         THREE.suppressExpoWarnings();
-        const count = useState(0);
-        const setCount = useState(0);
+
         return (
-                <View style={styles.container}>
                   <GraphicsView
                     onContextCreate={this.onContextCreate}
-                    onRender={this.onRender}
-                    >
-                </GraphicsView>
-                  <Text>You clicked {count} times.</Text>
-                    <Button
-                        onPress={() => setCount(count + 1)}
-                        title="Click me"
-                        color="red"
-                        accessibilityLabel="Click this button to increase count"
-                    />
-                </View>
+                    onRender={this.onRender}>
+                  </GraphicsView>
       );
     }
 
   // This is called by the `ExpoGraphics.View` once it's initialized
   onContextCreate = async ({
     gl,
-    canvas,
     width,
     height,
     scale: pixelRatio,
@@ -61,14 +46,14 @@ export default class App extends React.Component {
     this.scene = new THREE.Scene();
 
     this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    this.camera.position.z = 100;
+    this.camera.position.z = 40;
 
     //Create the final object to add to the scene
-    const geometry = new THREE.CylinderGeometry(5.9, 5.9, 1, 32);
-    const geometry1 = new THREE.CylinderGeometry(6, 6, 0.99, 32);
+    const geometry = new THREE.CylinderGeometry(5.89, 5.89, 1, 32);
+    const geometry1 = new THREE.CylinderGeometry(6, 6, 0.9, 32);
 
     const material = new THREE.MeshPhysicalMaterial({color: 0xF4D03F,});
-    const material1 = new THREE.MeshPhysicalMaterial({color: 0x61462A,});
+    const material1 = new THREE.MeshPhysicalMaterial({color: 0xFCEC33,});
     
     this.coin = new THREE.Mesh(geometry, material);
     this.coinEdge = new THREE.Mesh(geometry1, material1);
@@ -78,7 +63,7 @@ export default class App extends React.Component {
     this.scene.add(new THREE.AmbientLight(0x404040));
 
     const light = new THREE.DirectionalLight(0xffffff, 0.5);
-    light.position.set(-4, 4, 10);
+    light.position.set(0, 4, 10);
     this.scene.add(light);
 
     var y = new THREE.Vector3(0,1,0);
@@ -95,6 +80,7 @@ export default class App extends React.Component {
   onRender = delta => {
         
         this.coinEdge.rotation.x += 3.5 * delta;
+        this.coin.rotation.x += 3.5 * delta;
         this.renderer.render(this.scene, this.camera);
   }
 
@@ -115,5 +101,5 @@ export default class App extends React.Component {
       color: '#333333',
       marginBottom: 5
     }
-  });*/
+  });
 }
