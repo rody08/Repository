34f5c0bf69532141e9fd9dfd4f3bb9d6946a7bcc
Elwjaramillo <61395567+Elwjaramillo@users.noreply.github.com>
2020-04-24 00:28:32 -0500

    spinner

--- src/screens/SpinnerResults.js
+++ src/screens/SpinnerResults.js
@@ -65,10 +65,10 @@ function SpinnerResults({navigation}) {
       <PieChart style={styles.PieChart} data={pieData}/> 
 
       <Text style={styles.results}>
-        Red: {red}{"\n"}
-        Blue: {blue}{"\n"}
-        Green: {green}{"\n"}
-        Yellow: {yellow}
+        # 1:  {red}{"\n"}
+        #2:  {blue}{"\n"}
+        #3:  {green}{"\n"}
+        #4:  {yellow}
       </Text>
 
       {/*Play Again Button*/}
